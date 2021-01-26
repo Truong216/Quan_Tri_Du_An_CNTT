161d667c5312f96c92e5eae788ab668ae684b8e6
nguyen-tuan-nghia <tuannghia1406@gmail.com>
2021-01-26 21:52:10 +0700

    dang nhap

---
+++ logout.php
@@ -0,0 +1,14 @@
+﻿<?php 
+session_start(); 
+unset($_SESSION['phanquyen']);
+unset($_SESSION['user']);
+unset($_SESSION['idnd']);
+
+
+echo "
+							<script language='javascript'>
+								alert('Thoát  thành công');
+								window.open('../index.php','_self', 1);
+							</script>
+						";
+?>
