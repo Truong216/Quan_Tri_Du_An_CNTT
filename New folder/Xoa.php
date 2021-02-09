64d77dc1bc8fe136b97ecad077e0a7e4807d00fe
nguyen-tuan-nghia <tuannghia1406@gmail.com>
2021-02-09 22:29:40 +0700

    del

--- New folder/Xoa.php
+++
@@ -1,6 +0,0 @@
-<?php
-		include "connect.php";
-        $sql = "DELETE FROM wp_giohangnhanvien WHERE id ='$_GET[id]'";
-        mysqli_query($link,$sql);
-        header('location:GioHang.php');
-?>
--- Xoa.php
+++
@@ -1,6 +0,0 @@
-<?php
-		include "connect.php";
-        $sql = "DELETE FROM wp_giohang WHERE id ='$_GET[id]'";
-        mysqli_query($link,$sql);
-        header('location:GioHang.php');
-?>
